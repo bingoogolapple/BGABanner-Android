669dc03d721cdb447a22e72c52931da66c65aac5
bingoogolapple <bingoogolapple@gmail.com>
2017-01-13 23:15:18 +0800

    fix #76

--- demo/src/main/java/cn/bingoogolapple/bgabanner/demo/ui/activity/FrescoDemoActivity.java
+++ demo/src/main/java/cn/bingoogolapple/bgabanner/demo/ui/activity/FrescoDemoActivity.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.widget.Toast;
 
 import com.facebook.drawee.view.SimpleDraweeView;
@@ -28,18 +29,20 @@ public class FrescoDemoActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fresco_demo);
+        setTitle("FrescoDemo");
 
         mContentBanner = (BGABanner) findViewById(R.id.banner_fresco_demo_content);
-        mContentBanner.setDelegate(new BGABanner.Delegate<SimpleDraweeView, String>() {
+        mContentBanner.setDelegate(new BGABanner.Delegate<CardView, String>() {
             @Override
-            public void onBannerItemClick(BGABanner banner, SimpleDraweeView itemView, String model, int position) {
+            public void onBannerItemClick(BGABanner banner, CardView itemView, String model, int position) {
                 Toast.makeText(banner.getContext(), "点击了第" + (position + 1) + "页", Toast.LENGTH_SHORT).show();
             }
         });
-        mContentBanner.setAdapter(new BGABanner.Adapter<SimpleDraweeView, String>() {
+        mContentBanner.setAdapter(new BGABanner.Adapter<CardView, String>() {
             @Override
-            public void fillBannerItem(BGABanner banner, SimpleDraweeView itemView, String model, int position) {
-                itemView.setImageURI(Uri.parse(model));
+            public void fillBannerItem(BGABanner banner, CardView itemView, String model, int position) {
+                SimpleDraweeView simpleDraweeView = (SimpleDraweeView) itemView.findViewById(R.id.sdv_item_fresco_content);
+                simpleDraweeView.setImageURI(Uri.parse(model));
             }
         });
 
@@ -47,7 +50,8 @@ public class FrescoDemoActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<BannerModel> call, Response<BannerModel> response) {
                 BannerModel bannerModel = response.body();
-                mContentBanner.setData(R.layout.item_fresco, bannerModel.imgs, bannerModel.tips);
+//                mContentBanner.setData(R.layout.item_fresco, bannerModel.imgs, bannerModel.tips);
+                mContentBanner.setData(R.layout.item_fresco, bannerModel.imgs, null);
             }
 
             @Override
