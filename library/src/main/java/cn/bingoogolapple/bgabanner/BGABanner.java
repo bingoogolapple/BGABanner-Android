828207f4ab16776201b9a38893da03fec5f8142c
bingoogolapple <bingoogolapple@gmail.com>
2018-01-24 00:45:27 +0800

    fix #154

--- library/src/main/java/cn/bingoogolapple/bgabanner/BGABanner.java
+++ library/src/main/java/cn/bingoogolapple/bgabanner/BGABanner.java
@@ -383,7 +383,7 @@ public class BGABanner extends RelativeLayout implements BGAViewPager.AutoPlayDe
      * @param resIds         每一页图片资源 id
      */
     public void setData(@Nullable BGALocalImageSize localImageSize, @Nullable ImageView.ScaleType scaleType, @DrawableRes int... resIds) {
-        if (localImageSize != null) {
+        if (localImageSize == null) {
             localImageSize = new BGALocalImageSize(720, 1280, 320, 640);
         }
         if (scaleType != null) {
